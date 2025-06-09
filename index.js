ccae35f11139434989b9e540c31ba5451db8afcb
ManmohitSingh <dhimanmohit408@gmail.com>
2025-06-09 18:09:20 +0100

    Major changes made - added SSR

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ import express from 'express'
 const app = express()
 import { router as urlRoute } from './routes/url.js'
 import { connectToMongoDB } from './connection.js';
-import URL from './models/url.js'
+
+import path from "path"
+import staticRoute from './routes/staticRouter.js'
 const PORT = 8001;
 
-connectToMongoDB("mongodb://127.0.0.1:27017/urlshorterner")
+connectToMongoDB("mongodb://127.0.0.1:27017/urlshorterner-SSR")
     .then(() => console.log("MongoDB connected!"))
 
-//Middleware
+//humne express ko bta diya ha ki humne ek view engine use krna ha SSR ke liye or hum ejs use krenge
+app.set("view engine", "ejs");
+//Hum yaha par express ko bta rahe ha ki hamari jitni bhi ejs ki files ha wo sari ./views folder me padi ha 
+app.set("views",path.resolve("./views"))
+
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use('/',staticRoute);
+
 
 app.use("/url", urlRoute)
 
